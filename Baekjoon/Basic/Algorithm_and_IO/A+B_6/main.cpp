511e7a478bbccb38f9ea034f3f06e29f914764cc
Lahavas <uv187sn@naver.com>
2017-09-10 11:12:49 +0900

    Clear A+B_6

---
+++ Baekjoon/Basic/Algorithm_and_IO/A+B_6/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main(void)
+{
+    int cnt;
+    scanf("%d", &cnt);
+
+    while (cnt--) {
+        int a, b;
+        scanf("%d,%d", &a, &b);
+        printf("%d\n", a + b);
+    }
+
+    return 0;
+}
