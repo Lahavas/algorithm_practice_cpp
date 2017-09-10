aafe230ee5aa695cde012f7ba826be6d0dd7698f
Lahavas <uv187sn@naver.com>
2017-09-10 11:21:41 +0900

    Clear A+B_8

---
+++ Baekjoon/Basic/Algorithm_and_IO/A+B_8/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main(void)
+{
+    int cnt;
+    scanf("%d", &cnt);
+
+    for (int i = 1; i <= cnt; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i, a, b, a + b);
+    }
+
+    return 0;
+}
