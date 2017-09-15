8cd1629ebbab3018b1b1a7366a5a26d82a8707f1
Lahavas <uv187sn@naver.com>
2017-09-15 14:23:37 +0900

    Clear Search for Alphabet

---
+++ Baekjoon/Basic/Data_Structure_1st/Search_for_Alphabet/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cstring>
+
+#define MAX 100
+
+int main() {
+    char input[MAX];
+    scanf("%s", input);
+
+    for ( int i = 'a' ; i <= 'z'; i++ ) {
+        int index = -1;
+
+        for ( int j = 0; j < strlen(input); j++ ) {
+            if ( i == input[j] ) {
+                index = j;
+                break;
+            }
+        }
+        
+        printf("%d ", index);
+    }
+
+    return 0;
+}
