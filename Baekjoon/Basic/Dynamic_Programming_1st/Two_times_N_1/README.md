1aa7b82e005afe7007a3de46ad4b8562ddfbedbe
Lahavas <uv187sn@naver.com>
2017-10-02 13:57:31 +0900

    Update README

--- Baekjoon/Basic/Dynamic_Programming_1st/Two_times_N_1/README.md
+++ Baekjoon/Basic/Dynamic_Programming_1st/Two_times_N_1/README.md
@@ -1,3 +1,45 @@
 11726번 2xn 타일링
 ----------------
 
+2. Bottom-up 방식으로 풀이
+
+~~~ cpp
+#include <cstdio>
+
+#define MAX 1001
+#define DIVIDE 10007
+
+int arr[MAX] = { 0 };
+
+void twoTimesN(int n) {
+
+    for ( int i = 1; i <= n; i++ ) {
+
+        if ( i == 1 ) {
+            arr[i] = 1;
+        } else if ( i == 2) {
+            arr[i] = 2;
+        } else {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        if ( arr[i] > DIVIDE ) {
+            arr[i] %= DIVIDE;
+        }
+    }
+
+    return;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    twoTimesN(n);
+
+    int result = arr[n];
+    printf("%d\n", result);
+
+    return 0;
+}
+~~~
--- Baekjoon/Basic/Dynamic_Programming_1st/Two_times_N_1/main.cpp
+++ Baekjoon/Basic/Dynamic_Programming_1st/Two_times_N_1/main.cpp
@@ -5,27 +5,33 @@
 
 int arr[MAX] = { 0 };
 
-int twoTimesN(int n) {
+void twoTimesN(int n) {
 
-    arr[1] = 1;
-    arr[2] = 2;
+    for ( int i = 1; i <= n; i++ ) {
 
-    for ( int i = 3; i <= n; i++ ) {
-        arr[i] = arr[i - 1] + arr[i - 2];
+        if ( i == 1 ) {
+            arr[i] = 1;
+        } else if ( i == 2) {
+            arr[i] = 2;
+        } else {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
 
         if ( arr[i] > DIVIDE ) {
             arr[i] %= DIVIDE;
         }
     }
 
-    return arr[n];
+    return;
 }
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    int result = twoTimesN(n);
+    twoTimesN(n);
+
+    int result = arr[n];
     printf("%d\n", result);
 
     return 0;
