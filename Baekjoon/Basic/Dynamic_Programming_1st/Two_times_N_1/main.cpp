6fefd7ffab6f9ba726de8db3d1180e4ac132be0e
Lahavas <uv187sn@naver.com>
2017-10-02 13:50:34 +0900

    Clear Two times N 1

--- Baekjoon/Basic/Dynamic_Programming_1st/Two_times_N_1/main.cpp
+++ Baekjoon/Basic/Dynamic_Programming_1st/Two_times_N_1/main.cpp
@@ -1,7 +1,32 @@
-#include <iostream>
+#include <cstdio>
 
-#define divide 10007
+#define MAX 1001
+#define DIVIDE 10007
+
+int arr[MAX] = { 0 };
+
+int twoTimesN(int n) {
+
+    arr[1] = 1;
+    arr[2] = 2;
+
+    for ( int i = 3; i <= n; i++ ) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+
+        if ( arr[i] > DIVIDE ) {
+            arr[i] %= DIVIDE;
+        }
+    }
+
+    return arr[n];
+}
 
 int main() {
-    
+    int n;
+    scanf("%d", &n);
+
+    int result = twoTimesN(n);
+    printf("%d\n", result);
+
+    return 0;
 }
