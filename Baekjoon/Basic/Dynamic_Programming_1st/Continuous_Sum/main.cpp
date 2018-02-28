3775768e9e95ceb4e57592593572d4b43cec6d14
Lahavas <uv187sn@naver.com>
2018-02-28 15:44:05 +0900

    Fix Source

--- Baekjoon/Basic/Dynamic_Programming_1st/Continuous_Sum/main.cpp
+++ Baekjoon/Basic/Dynamic_Programming_1st/Continuous_Sum/main.cpp
@@ -16,26 +16,15 @@ int main()
     }
     
     vector<int> d(n, 0);
+    
     for ( int i = 0; i < n; ++i ) {
         d[i] = a[i];
-        if ( d[i] < 0 ) {
+        if ( i == 0 ) {
             continue;
         }
         
-        for ( int j = i - 1; j >= 0; --j ) {
-            if ( a[j] >= 0 ) {
-                d[i] += a[j];
-            } else {
-                break;
-            }
-        }
-        
-        for ( int j = i + 1; j < n; ++j ) {
-            if ( a[j] >= 0 ) {
-                d[i] += a[j];
-            } else {
-                break;
-            }
+        if ( d[i] < d[i - 1] + a[i] ) {
+            d[i] = d[i - 1] + a[i];
         }
     }
     
