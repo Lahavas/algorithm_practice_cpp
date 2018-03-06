2cc63aa141a6199f456393354cf68604aa47966c
Lahavas <uv187sn@naver.com>
2018-03-06 16:41:05 +0900

    Clear 좌표 정렬하기 2

--- Baekjoon/Basic/Sorting_1st/README.md
+++ Baekjoon/Basic/Sorting_1st/README.md
@@ -8,7 +8,7 @@ Baekjoon Online Judge 초급 Ch05. 정렬
 
 * [수 정렬하기 2](./수_정렬하기_2) [[2751번 상세보기](https://www.acmicpc.net/problem/2751)]
 * [좌표 정렬하기](./좌표_정렬하기) [[11650번 상세보기](https://www.acmicpc.net/problem/11650)]
-* 좌표 정렬하기 2 [[11651번 상세보기](https://www.acmicpc.net/problem/11651)]
+* [좌표 정렬하기 2](./좌표_정렬하기_2) [[11651번 상세보기](https://www.acmicpc.net/problem/11651)]
 * 나이순 정렬 [[10814번 상세보기](https://www.acmicpc.net/problem/10814)]
 * 국영수 [[10825번 상세보기](https://www.acmicpc.net/problem/10825)]
 * 수 정렬하기 3 [[10989번 상세보기](https://www.acmicpc.net/problem/10989)]
---
+++ Baekjoon/Basic/Sorting_1st/좌표_정렬하기_2/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    
+    vector<pair<int, int>> a(n);
+    for ( int i = 0; i < n; ++i ) {
+        scanf("%d %d", &a[i].first, &a[i].second);
+    }
+    
+    sort(a.begin(), a.end(), 
+        [](const pair<int, int> &u, const pair<int, int> &v) {
+           return u.second == v.second ? u.first < v.first : u.second < v.second; 
+        });
+    
+    for ( auto &p : a ) {
+        printf("%d %d\n", p.first, p.second);
+    }
+}
