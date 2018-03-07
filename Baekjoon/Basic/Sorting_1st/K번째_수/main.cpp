c31bd5c59d46c7cf0691c2ddb84d3d1bc8e4495e
Lahavas <uv187sn@naver.com>
2018-03-08 00:35:48 +0900

    Clear K번째 수

---
+++ Baekjoon/Basic/Sorting_1st/K번째_수/main.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    int k = 0;
+    scanf("%d %d", &n, &k);
+    --k;
+    
+    vector<long> a(n, 0);
+    for ( int i = 0; i < n; ++i ) {
+        scanf("%ld", &a[i]);
+    }
+    
+    nth_element(a.begin(), a.begin() + k, a.end());
+    printf("%ld\n", a[k]);
+    
+    return 0;
+}
